5a96faf13e961711558b3ccc7330fb16b9803305
Nick Kathios <njk2136@columbia.edu>
2022-04-11 08:35:13 -0400

    updates

--- scripts/average_connectivity_firstlevel.m
+++ scripts/average_connectivity_firstlevel.m
@@ -1,127 +1,127 @@
 %Isolate the auditory-auditory section of the matrix
-childhood_auditory = Z(,1:18,:);
+adulthood_auditory = Z(1:18,1:18,:);
 
 %Isolate the top half of the corr matrix for each liking rating (aud-aud)
-for sub = 1:18
+for sub = 1:21
     
-    temp = childhood_auditory(:,:,sub);
-    childhood_auditory_byparticipant(:,sub) = temp(find(~tril(ones(size(temp)))));
-    childhood_auditory_participant_mean = mean(childhood_auditory_byparticipant);    
+    temp = adulthood_auditory(:,:,sub);
+    adulthood_auditory_byparticipant(:,sub) = temp(find(~tril(ones(size(temp)))));
+    adulthood_auditory_participant_mean = mean(adulthood_auditory_byparticipant);    
 end
-childhood_auditory_participant_mean = childhood_auditory_participant_mean'
+adulthood_auditory_participant_mean = adulthood_auditory_participant_mean'
 
 %Isolate the reward-reward section of the matrix
-childhood_reward = Z(19:36,19:36,:);
+adulthood_reward = Z(19:36,19:36,:);
 
 %Isolate the top half of the corr matrix for each liking rating (aud-aud)
-for sub = 1:18
+for sub = 1:21
     
-    temp = childhood_reward(:,:,sub);
-    childhood_reward_byparticipant(:,sub) = temp(find(~tril(ones(size(temp)))));
-    childhood_reward_participant_mean = mean(childhood_reward_byparticipant);    
+    temp = adulthood_reward(:,:,sub);
+    adulthood_reward_byparticipant(:,sub) = temp(find(~tril(ones(size(temp)))));
+    adulthood_reward_participant_mean = mean(adulthood_reward_byparticipant);    
 end
-childhood_reward_participant_mean = childhood_reward_participant_mean'
+adulthood_reward_participant_mean = adulthood_reward_participant_mean'
 
 %Isolate the auditory-reward section of the matrix
-childhood_reward_auditory = Z(1:18,19:36,:);
+adulthood_reward_auditory = Z(1:18,19:36,:);
 
 %Get the mean for each participant across the matrix (aud-rew)
-for sub = 1:18
+for sub = 1:21
     
-    childhood_auditory_reward_mean(:,sub) = mean(childhood_reward_auditory(:,:,sub), 'all');
+    adulthood_auditory_reward_mean(:,sub) = mean(adulthood_reward_auditory(:,:,sub), 'all');
 end
 
-childhood_auditory_reward_mean = childhood_auditory_reward_mean'
+adulthood_auditory_reward_mean = adulthood_auditory_reward_mean'
 
 
 %mpfc - auditory + reward
-childhood_mpfc_auditory_reward=Z(1:36,31,:);
+adulthood_mpfc_auditory_reward=Z(1:36,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:18
+for sub = 1:21
     
-    childhood_mpfc_auditory_reward_mean(:,sub) = nanmean(childhood_mpfc_auditory_reward(:,:,sub), 'all');
+    adulthood_mpfc_auditory_reward_mean(:,sub) = nanmean(adulthood_mpfc_auditory_reward(:,:,sub), 'all');
 end
-childhood_mpfc_auditory_reward_mean = childhood_mpfc_auditory_reward_mean'
+adulthood_mpfc_auditory_reward_mean = adulthood_mpfc_auditory_reward_mean'
 
 %mpfc - auditory
-childhood_mpfc_auditory=Z(1:18,31,:);
+adulthood_mpfc_auditory=Z(1:18,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:18
+for sub = 1:21
     
-    childhood_mpfc_auditory_mean(:,sub) = nanmean(childhood_mpfc_auditory(:,:,sub), 'all');
+    adulthood_mpfc_auditory_mean(:,sub) = nanmean(adulthood_mpfc_auditory(:,:,sub), 'all');
 end
-childhood_mpfc_auditory_mean = childhood_mpfc_auditory_mean'
+adulthood_mpfc_auditory_mean = adulthood_mpfc_auditory_mean'
 
 %mpfc - reward
-childhood_mpfc_reward=Z(19:36,31,:);
+adulthood_mpfc_reward=Z(19:36,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:18
+for sub = 1:21
     
-    childhood_mpfc_reward_mean(:,sub) = nanmean(childhood_mpfc_reward(:,:,sub), 'all');
+    adulthood_mpfc_reward_mean(:,sub) = nanmean(adulthood_mpfc_reward(:,:,sub), 'all');
 end
-childhood_mpfc_reward_mean = childhood_mpfc_reward_mean'
+adulthood_mpfc_reward_mean = adulthood_mpfc_reward_mean'
 
 %mpfc - vstr
-childhood_mpfc_nac=Z(29:30,31,:);
+adulthood_mpfc_nac=Z(29:30,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:18
+for sub = 1:21
     
-    childhood_mpfc_nac_mean(:,sub) = nanmean(childhood_mpfc_nac(:,:,sub), 'all');
+    adulthood_mpfc_nac_mean(:,sub) = nanmean(adulthood_mpfc_nac(:,:,sub), 'all');
 end
-childhood_mpfc_nac_mean = childhood_mpfc_nac_mean'
+adulthood_mpfc_nac_mean = adulthood_mpfc_nac_mean'
 
 %mpfc - str
-childhood_mpfc_str=Z([19:20,29:30],31,:);
+adulthood_mpfc_str=Z([19:20,29:30],31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:18
+for sub = 1:21
     
-    childhood_mpfc_str_mean(:,sub) = nanmean(childhood_mpfc_str(:,:,sub), 'all');
+    adulthood_mpfc_str_mean(:,sub) = nanmean(adulthood_mpfc_str(:,:,sub), 'all');
 end
-childhood_mpfc_str_mean = childhood_mpfc_str_mean'
+adulthood_mpfc_str_mean = adulthood_mpfc_str_mean'
 
 %str
-childhood_str=Z(19:20,29:30,:);
+adulthood_str=Z(19:20,29:30,:);
 
 %Get the mean for each participant across the matrix
-for sub = 1:18
+for sub = 1:21
     
-    childhood_str_mean(:,sub) = nanmean(childhood_str(:,:,sub), 'all');
+    adulthood_str_mean(:,sub) = nanmean(adulthood_str(:,:,sub), 'all');
 end
-childhood_str_mean = childhood_str_mean'
+adulthood_str_mean = adulthood_str_mean'
 
 %str - auditory
-childhood_str_auditory=Z([19:20,29:30], 1:18,:);
+adulthood_str_auditory=Z([19:20,29:30], 1:18,:);
 
 %Get the mean for each participant across the matrix
-for sub = 1:18
+for sub = 1:21
     
-    childhood_str_auditory_mean(:,sub) = nanmean(childhood_str_auditory(:,:,sub), 'all');
+    adulthood_str_auditory_mean(:,sub) = nanmean(adulthood_str_auditory(:,:,sub), 'all');
 end
-childhood_str_auditory_mean = childhood_str_auditory_mean'
+adulthood_str_auditory_mean = adulthood_str_auditory_mean'
 
 %nac - auditory
-childhood_nac_auditory=Z(29:30, 1:18,:);
+adulthood_nac_auditory=Z(29:30, 1:18,:);
 
 %Get the mean for each participant across the matrix
-for sub = 1:18
+for sub = 1:21
     
-    childhood_nac_auditory_mean(:,sub) = nanmean(childhood_nac_auditory(:,:,sub), 'all');
+    adulthood_nac_auditory_mean(:,sub) = nanmean(adulthood_nac_auditory(:,:,sub), 'all');
 end
-childhood_nac_auditory_mean = childhood_nac_auditory_mean'
+adulthood_nac_auditory_mean = adulthood_nac_auditory_mean'
 
-writematrix(childhood_auditory_participant_mean, 'childhood_auditory_participant_mean.csv')
-writematrix(childhood_reward_participant_mean, 'childhood_reward_participant_mean.csv')
-writematrix(childhood_auditory_participant_mean, 'childhood_auditory_participant_mean.csv')
-writematrix(childhood_auditory_reward_mean, 'childhood_reward_auditory_mean.csv')
-writematrix(childhood_mpfc_auditory_reward_mean, 'childhood_mpfc_auditory_reward_mean.csv')
-writematrix(childhood_mpfc_auditory_mean, 'childhood_mpfc_auditory_mean.csv')
-writematrix(childhood_mpfc_reward_mean, 'childhood_mpfc_reward_mean.csv')
-writematrix(childhood_mpfc_nac_mean, 'childhood_mpfc_nac.csv')
-writematrix(childhood_mpfc_str_mean, 'childhood_mpfc_str.csv')
-writematrix(childhood_str_auditory_mean, 'childhood_str_auditory_mean.csv')
-writematrix(childhood_nac_auditory_mean, 'childhood_nac_auditory_mean.csv')
+writematrix(adulthood_auditory_participant_mean, 'adulthood_auditory_participant_mean.csv')
+writematrix(adulthood_reward_participant_mean, 'adulthood_reward_participant_mean.csv')
+writematrix(adulthood_auditory_participant_mean, 'adulthood_auditory_participant_mean.csv')
+writematrix(adulthood_auditory_reward_mean, 'adulthood_reward_auditory_mean.csv')
+writematrix(adulthood_mpfc_auditory_reward_mean, 'adulthood_mpfc_auditory_reward_mean.csv')
+writematrix(adulthood_mpfc_auditory_mean, 'adulthood_mpfc_auditory_mean.csv')
+writematrix(adulthood_mpfc_reward_mean, 'adulthood_mpfc_reward_mean.csv')
+writematrix(adulthood_mpfc_nac_mean, 'adulthood_mpfc_nac.csv')
+writematrix(adulthood_mpfc_str_mean, 'adulthood_mpfc_str.csv')
+writematrix(adulthood_str_auditory_mean, 'adulthood_str_auditory_mean.csv')
+writematrix(adulthood_nac_auditory_mean, 'adulthood_nac_auditory_mean.csv')
