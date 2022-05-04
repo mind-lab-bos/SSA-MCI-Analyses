d10988e2844e1cb12b86748975c0f1e067fd1873
Nick Kathios <njk2136@columbia.edu>
2022-05-04 13:27:42 -0400

    updates

--- scripts/average_connectivity_firstlevel.m
+++ scripts/average_connectivity_firstlevel.m
@@ -2,7 +2,7 @@
 adulthood_auditory = Z(1:18,1:18,:);
 
 %Isolate the top half of the corr matrix for each liking rating (aud-aud)
-for sub = 1:21
+for sub = 1:25
     
     temp = adulthood_auditory(:,:,sub);
     adulthood_auditory_byparticipant(:,sub) = temp(find(~tril(ones(size(temp)))));
@@ -14,7 +14,7 @@ adulthood_auditory_participant_mean = adulthood_auditory_participant_mean'
 adulthood_reward = Z(19:36,19:36,:);
 
 %Isolate the top half of the corr matrix for each liking rating (aud-aud)
-for sub = 1:21
+for sub = 1:25
     
     temp = adulthood_reward(:,:,sub);
     adulthood_reward_byparticipant(:,sub) = temp(find(~tril(ones(size(temp)))));
@@ -26,7 +26,7 @@ adulthood_reward_participant_mean = adulthood_reward_participant_mean'
 adulthood_reward_auditory = Z(1:18,19:36,:);
 
 %Get the mean for each participant across the matrix (aud-rew)
-for sub = 1:21
+for sub = 1:25
     
     adulthood_auditory_reward_mean(:,sub) = mean(adulthood_reward_auditory(:,:,sub), 'all');
 end
@@ -38,7 +38,7 @@ adulthood_auditory_reward_mean = adulthood_auditory_reward_mean'
 adulthood_mpfc_auditory_reward=Z(1:36,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:21
+for sub = 1:25
     
     adulthood_mpfc_auditory_reward_mean(:,sub) = nanmean(adulthood_mpfc_auditory_reward(:,:,sub), 'all');
 end
@@ -48,7 +48,7 @@ adulthood_mpfc_auditory_reward_mean = adulthood_mpfc_auditory_reward_mean'
 adulthood_mpfc_auditory=Z(1:18,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:21
+for sub = 1:25
     
     adulthood_mpfc_auditory_mean(:,sub) = nanmean(adulthood_mpfc_auditory(:,:,sub), 'all');
 end
@@ -58,7 +58,7 @@ adulthood_mpfc_auditory_mean = adulthood_mpfc_auditory_mean'
 adulthood_mpfc_reward=Z(19:36,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:21
+for sub = 1:25
     
     adulthood_mpfc_reward_mean(:,sub) = nanmean(adulthood_mpfc_reward(:,:,sub), 'all');
 end
@@ -68,7 +68,7 @@ adulthood_mpfc_reward_mean = adulthood_mpfc_reward_mean'
 adulthood_mpfc_nac=Z(29:30,31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:21
+for sub = 1:25
     
     adulthood_mpfc_nac_mean(:,sub) = nanmean(adulthood_mpfc_nac(:,:,sub), 'all');
 end
@@ -78,7 +78,7 @@ adulthood_mpfc_nac_mean = adulthood_mpfc_nac_mean'
 adulthood_mpfc_str=Z([19:20,29:30],31,:);
 
 %Get the mean for each participant across the matrix (mpfc-aud/rew)
-for sub = 1:21
+for sub = 1:25
     
     adulthood_mpfc_str_mean(:,sub) = nanmean(adulthood_mpfc_str(:,:,sub), 'all');
 end
@@ -88,7 +88,7 @@ adulthood_mpfc_str_mean = adulthood_mpfc_str_mean'
 adulthood_str=Z(19:20,29:30,:);
 
 %Get the mean for each participant across the matrix
-for sub = 1:21
+for sub = 1:25
     
     adulthood_str_mean(:,sub) = nanmean(adulthood_str(:,:,sub), 'all');
 end
@@ -98,7 +98,7 @@ adulthood_str_mean = adulthood_str_mean'
 adulthood_str_auditory=Z([19:20,29:30], 1:18,:);
 
 %Get the mean for each participant across the matrix
-for sub = 1:21
+for sub = 1:25
     
     adulthood_str_auditory_mean(:,sub) = nanmean(adulthood_str_auditory(:,:,sub), 'all');
 end
@@ -108,7 +108,7 @@ adulthood_str_auditory_mean = adulthood_str_auditory_mean'
 adulthood_nac_auditory=Z(29:30, 1:18,:);
 
 %Get the mean for each participant across the matrix
-for sub = 1:21
+for sub = 1:25
     
     adulthood_nac_auditory_mean(:,sub) = nanmean(adulthood_nac_auditory(:,:,sub), 'all');
 end
